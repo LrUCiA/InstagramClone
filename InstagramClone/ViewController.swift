8b7f58c1367568e02a01be51d2305afa81611db5
LrUCiA <crosshertz7@gmail.com>
2022-05-21 11:40:06 +0900

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by LrUCiA on 2022/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
